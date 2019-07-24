752dcb6d4ad425e072c755929bf496531fba79ef
Jiadan <jiadanwang@gmail.com>
2019-07-24 21:30:32 +0000

    create tables

---
+++ migrations/01_schema.sql
@@ -0,0 +1,43 @@
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  thumbnail_photo_url VARCHAR(255),
+  cost_per_night VARCHAR(255),
+  street VARCHAR(255),
+  parking_spaces VARCHAR(255),
+  number_of_bathrooms VARCHAR(255),
+  number_of_bedrooms VARCHAR(255),
+  country VARCHAR(255),
+  -- street
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  active boolean
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE,
+  end_date DATE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  rating VARCHAR(255),
+  message VARCHAR(255)
+
+);
